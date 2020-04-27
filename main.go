0072bbcb5103f3bc9282951cc477fd17f816b2ac
jxuamazon <48922441+jxuamazon@users.noreply.github.com>
2020-04-26 22:58:51 -0400

    add telescope

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello from Space!"}
+		res := &response{Message: "Hello from Space Telescope!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
