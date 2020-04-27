64720f9f9aace9ea66309a087f0e94896af2997e
jxuamazon <48922441+jxuamazon@users.noreply.github.com>
2020-04-26 21:33:07 -0400

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello from Space Telescope!"}
+		res := &response{Message: "Hello from Space!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
