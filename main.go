46b45ed3e62896c717f2ce13b7cdfac7ae9720d4
jxuamazon <48922441+jxuamazon@users.noreply.github.com>
2020-04-29 17:16:49 -0400

    add workshop

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello from Space Telescope Again !"}
+		res := &response{Message: "Hello from Space Telescope Workshop !"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
