6eb0bdef2be9874056db6d18cec270c8bffa8bb5
jxuamazon <48922441+jxuamazon@users.noreply.github.com>
2020-04-30 11:26:43 -0400

    change text to AWS

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello from Space Telescope Workshop !"}
+		res := &response{Message: "Hello from AWS !"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
